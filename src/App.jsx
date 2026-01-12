a18d324ab698679bbb243d35dc4ed2b6e1fa78ec
Swastibiharinayak <nayakswastibihari@gmail.com>
2026-01-12 19:44:48 +0530

    chore: initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ExpenseForm from './components/ExpenseForm'
+import './index.css'
+
+const App = () => {
+  return (
+    <div>
+      <ExpenseForm/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ExpenseForm = () => {
+  return (
+    <div className='bg-orange-700 w-screen h-screen'>
+        
+        
+    </div>
+  )
+}
+
+export default ExpenseForm
