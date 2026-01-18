d0d1321883091232ed388a3aabb79c3412d93a34
Swastibiharinayak <nayakswastibihari@gmail.com>
2026-01-18 10:30:44 +0530

    chore: minor UI tweaks

--- src/components/expense/ExpenseForm.jsx
+++ src/components/expense/ExpenseForm.jsx
@@ -2,26 +2,28 @@ import React from 'react'
 
 const ExpenseForm = () => {
   return (
-    <div className='bg-orange-700 w-screen h-screen'>
-        <form>
-          <label htmlFor="">Title</label>
-          <input type="text" />
+    <div className='w-full h-full'>
+      <h1 className='text-5xl font-mono font-medium p-5'>New Expense</h1>  
+      <hr />
+      <form className='flex flex-col items-center justify-center'>
+        <label htmlFor="">Title</label>
+        <input type="text" />
 
-          <label htmlFor="">Amount</label>
-          <input type="number" />
+        <label htmlFor="">Amount</label>
+        <input type="number" />
 
-          <label htmlFor="">Catagory</label>
-          <select name="" id="">
-            <option value="">Fashion</option>
-            <option value="">Food</option>
-            <option value="">Grocery</option>
-            <option value="">Travel</option>
-            <option value="">Miscellaneous</option>
-          </select>
+        <label htmlFor="">Catagory</label>
+        <select name="" id="">
+          <option value="">Fashion</option>
+          <option value="">Food</option>
+          <option value="">Grocery</option>
+          <option value="">Travel</option>
+          <option value="">Miscellaneous</option>
+        </select>
 
-          <label htmlFor="">Date</label>
-          <input type="date" name="" id="" />
-        </form>
+        <label htmlFor="">Date</label>
+        <input type="date" name="" id="" />
+      </form>
     </div>
   )
 }
