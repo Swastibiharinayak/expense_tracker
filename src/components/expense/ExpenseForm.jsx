b1f84c5a27558a96dee5d15e44ff782ce4afb2d7
Swastibiharinayak <nayakswastibihari@gmail.com>
2026-01-18 16:58:41 +0530

    ui: update expense form UI

--- src/components/expense/ExpenseForm.jsx
+++ src/components/expense/ExpenseForm.jsx
@@ -2,29 +2,84 @@ import React from 'react'
 
 const ExpenseForm = () => {
   return (
-    <div className='w-full h-full'>
-      <h1 className='text-5xl font-mono font-medium p-5'>New Expense</h1>  
-      <hr />
-      <form className='flex flex-col items-center justify-center'>
-        <label htmlFor="">Title</label>
-        <input type="text" />
+    <div className='w-full min-h-scree  p-8'>
+  <h1 className='text-4xl font-medium mb-4'>New Expense</h1>
+  <hr className="border-gray-700 mb-8"/>
 
-        <label htmlFor="">Amount</label>
-        <input type="number" />
+  <div className="grid grid-cols-1 md:grid-cols-3">
+    <form className="md:col-span-2 grid gap-y-5">
 
-        <label htmlFor="">Catagory</label>
-        <select name="" id="">
-          <option value="">Fashion</option>
-          <option value="">Food</option>
-          <option value="">Grocery</option>
-          <option value="">Travel</option>
-          <option value="">Miscellaneous</option>
+      <div className="grid grid-cols-3 items-center gap-x-6">
+        <label className="text-sm text-gray-400 text-right">Title</label>
+        <input
+          type="text"
+          className="col-span-2 bg-gray-800 border border-gray-700 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-teal-500"
+        />
+      </div>
+
+      <div className="grid grid-cols-3 items-center gap-x-6">
+        <label className="text-sm text-gray-400 text-right">Amount</label>
+        <input
+          type="number"
+          className="col-span-2 bg-gray-800 border border-gray-700 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-teal-500"
+        />
+      </div>
+
+      <div className="grid grid-cols-3 items-center gap-x-6">
+        <label className="text-sm text-gray-400 text-right">Catagory</label>
+        <select className="col-span-2 bg-gray-800 border border-gray-700 rounded-md px-4 py-2">
+          <option disabled selected>Select one</option>
+          <option>Fashion</option>
+          <option>Food</option>
+          <option>Grocery</option>
+          <option>Travel</option>
+          <option>Miscellaneous</option>
         </select>
+      </div>
 
-        <label htmlFor="">Date</label>
-        <input type="date" name="" id="" />
-      </form>
-    </div>
+      <div className="grid grid-cols-3 items-center gap-x-6">
+        <label className="text-sm text-gray-400 text-right">Date</label>
+        <input
+          type="date"
+          className="col-span-2 bg-gray-800 border border-gray-700 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-teal-500"
+        />
+      </div>
+
+      <div className="grid grid-cols-3 items-center gap-x-6">
+        <label className="text-sm text-gray-400 text-right">Payment type</label>
+        <select className="col-span-2 bg-gray-800 border border-gray-700 rounded-md px-4 py-2">
+          <option disabled selected>Select one</option>
+          <option>UPI</option>
+          <option>Cash</option>
+          <option>Credit card</option>
+        </select>
+      </div>
+
+      <div className="grid grid-cols-3 items-start gap-x-6">
+        <label className="text-sm text-gray-400 text-right pt-2">
+          Description
+        </label>
+        <textarea
+          rows={5}
+          className="col-span-2 bg-gray-800 border border-gray-700 rounded-md px-4 py-2"
+        ></textarea>
+      </div>
+
+      <div className="grid grid-cols-3">
+        <div className="col-span-2 flex justify-end gap-4 pt-4">
+          <button className="px-6 py-2 bg-gray-700 rounded-md hover:bg-gray-600">
+            Save
+          </button>
+          <button className="px-6 py-2 bg-gray-700 rounded-md hover:bg-gray-600">
+            Delete
+          </button>
+        </div>
+      </div>
+
+    </form>
+  </div>
+</div>
+
   )
 }
 
